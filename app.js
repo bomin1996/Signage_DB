6ec39459ecad2bd65b486812742416d7070a561b
bomin <bomin199628@gmail.com>
2024-05-17 09:46:10 +0900

    add DB connect

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const db = require('./db');  // 데이터베이스 연결 추가
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,5 +20,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// 연결 테스트용 간단한 쿼리
+db.query('SELECT 1 + 1 AS solution', (err, results, fields) => {
+    if (err) throw err;
+    console.log('The solution is: ', results[0].solution);
+});
 
 module.exports = app;
---
+++ db.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('데이터베이스 연결 실패:', err.stack);
+        return;
+    }
+    console.log('데이터베이스에 연결되었습니다. ID:', connection.threadId);
+});
+
+module.exports = connection;
