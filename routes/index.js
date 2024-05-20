304b27988b37cde5b565602cfe917eea180375aa
bomin <bomin199628@gmail.com>
2024-05-20 12:47:51 +0900

    add DB

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db');  // db 연결 모듈 가져오기
 
-/* GET home page. */
+// GET home page.
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+// GET databases
+router.get('/databases', function(req, res, next) {
+  db.query('SHOW DATABASES', (err, results) => {
+    if (err) {
+      return next(err);
+    }
+    res.json(results);
+  });
 });
 
 module.exports = router;
